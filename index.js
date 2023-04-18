3249bd4ab97a9216dc483747f5e79e58732f2706
tandycell <131168612+tandycell@users.noreply.github.com>
2023-04-18 20:15:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '1711711266:AAFwPspZUCeFsO9zCJ9UdPQP12XMnM7Qx9c';
+const token = '6217733959:AAGJkBDcXvdMZJYPutZvGXZlUSj8ZARSF8c';
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
